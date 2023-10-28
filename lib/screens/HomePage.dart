82ea359930801e3e4db15276f9f10164ca866a57
MahmoudDe <mahmoud.developer2@gmail.com>
2023-10-28 18:14:37 +0200

    modify nav bar and remake home page

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -2,12 +2,87 @@ import 'package:bdh/screens/statics/appBar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  final List<String> descriptions = [
+    'Alaa Shebany has borrowed a new story',
+    'Magmoud turn back a new story',
+    'admin has sent a new message for you',
+    'Alaa Shebany has borrowed a new story',
+    'Magmoud turn back a new story',
+    'admin has sent a new message for you',
+    'Alaa Shebany has borrowed a new story',
+    'Magmoud turn back a new story',
+    'admin has sent a new message for you',
+    'Alaa Shebany has borrowed a new story',
+    'Magmoud turn back a new story',
+    'admin has sent a new message for you',
+
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
-      body: Center(
-        child: Text(' Home Screen!'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 10.0),
+              child: Text(
+                'Notifications',
+                style: TextStyle(fontSize: 28, color: Colors.grey),
+              ),
+            ),
+            Flexible(
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+                elevation: 0.0,
+                color: Colors.white,
+                child: Container(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListView.separated(
+                      physics: const BouncingScrollPhysics(),
+                      itemCount: descriptions.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(top: 5.0),
+                          child: ListTile(
+                            leading: Container(
+                              width: 50.0,
+                              height: 60.0,
+                              decoration:  BoxDecoration(
+                                color: Colors.orange,
+                                border: Border.all(
+                                color: Colors.orange,
+                                width: 2.0,
+                              ),
+                              shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  image: AssetImage('assets/images/bdh_logo2.jpeg'),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            title: Text(
+                              descriptions[index],
+                              style: TextStyle(
+                                  fontFamily: 'Avenir',
+                                  color: Colors.grey.shade800),
+                            ),
+                          ),
+                        );
+                      },
+                      separatorBuilder: (context, index) {
+                        return const Divider();
+                      },
+                    )),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/statics/navBar.dart
+++ lib/screens/statics/navBar.dart
@@ -9,30 +9,48 @@ class NavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 70,
-      child: BottomNavigationBar(
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        iconSize: 25.0,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Iconsax.home),
-            label: '',
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 70,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.02),
+              spreadRadius: 0.2,
+              blurRadius: 10,
+              offset: Offset(0, 5),
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(30.0),
+          child: BottomNavigationBar(
+            backgroundColor: Colors.white,
+            elevation: 0.0,
+            iconSize: 25.0,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Iconsax.home),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Iconsax.search_normal),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Iconsax.setting),
+                label: '',
+              ),
+            ],
+            currentIndex: currentIndex,
+            onTap: onTap,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Iconsax.search_normal),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Iconsax.setting),
-            label: '',
-          ),
-        ],
-        currentIndex: currentIndex,
-        onTap: onTap,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
+        ),
       ),
     );
   }
